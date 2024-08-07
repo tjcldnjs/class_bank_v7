7e7848b77c0984da0115e6b72a32ecbd5036e377
tjcldnjs <167273757+tjcldnjs@users.noreply.github.com>
2024-08-07 11:17:33 +0900

    로그아웃 기능 추가

--- src/main/java/com/tenco/bank/controller/UserController.java
+++ src/main/java/com/tenco/bank/controller/UserController.java
@@ -7,19 +7,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tenco.bank.dto.SignInDTO;
 import com.tenco.bank.dto.SignUpDTO;
 import com.tenco.bank.handler.exception.DataDeliveryException;
+import com.tenco.bank.repository.model.User;
 import com.tenco.bank.service.UserService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller // Ioc 에 대상 (싱글톤 패턴으로 관리)
 @RequestMapping("/user") // 대문 처리
 public class UserController {
 	
 	private UserService userService;
+	private final HttpSession session;
 	
 	@Autowired // DI 처리      // 노란색 경고는 사용할 필요 없음 -  가독성을 위해서 선언해도 됨
-	public UserController(UserService service) {
+	public UserController(UserService service, HttpSession session) {
 		this.userService = service;
+		this.session = session;
 	}
 	
 	// 주소설계 -> http://localhost:8080/user/sign-up
@@ -66,8 +72,56 @@ public class UserController {
 		// 서비스 객체로 전달
 		userService.createUser(dto);
 		
-		// TODO - 추후 수정
+		// TODO - 코드 수정 완료
+		return "redirect:/user/sign-in";
+	}
+	
+	/*
+	 * 로그인 화면 요청
+	 * 주소설계 : http://localhost:8080/user/sign-in
+	 */
+	@GetMapping("/sign-in")
+	public String signInPage() {
+		// 인증검사  x
+		// 유효성검사  x
+		return "user/signIn";
+	}
+	
+	/**
+	 * 회원가입 요청처리
+	 * 주소설계 : http://localhost:8080/user/sign-in
+	 * @return
+	 */
+	@PostMapping("/sign-in")
+	public String signProc(SignInDTO dto) {
+		
+		// 1. 인증검사 x
+		// 2. 유효성 검사
+		
+		if(dto.getUsername() == null || dto.getUsername().isEmpty()) {
+			throw new DataDeliveryException("username을 입력하시오", HttpStatus.BAD_REQUEST);
+		}
+		
+		if(dto.getPassword() == null || dto.getPassword().isEmpty()) {
+			throw new DataDeliveryException("password를 입력하시오", HttpStatus.BAD_REQUEST);
+		}
+		
+		// 서비스 호출
+		User principal =  userService.readUser(dto);
+		
+		// 세션 메모리에 등록 처리
+		session.setAttribute("principal", principal);
+		
+		// 새로운 페이지로 이동 처리
+		// TODO - 계좌 목록 페이지 이동처리 예정
 		return "redirect:/index";
+	}
+	
+	@GetMapping("/logout")
+	public String logout() {
+		// HTTP 세션을 무효화 (세션의 모든 데이터가 제거되고 세션이 종료)
+		session.invalidate(); 
+		return "redirect:/user/sign-in";
 	}
 
 }
--- src/main/java/com/tenco/bank/service/UserService.java
+++ src/main/java/com/tenco/bank/service/UserService.java
@@ -6,10 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.tenco.bank.dto.SignInDTO;
 import com.tenco.bank.dto.SignUpDTO;
 import com.tenco.bank.handler.exception.DataDeliveryException;
 import com.tenco.bank.handler.exception.RedirectException;
 import com.tenco.bank.repository.interfaces.UserRepository;
+import com.tenco.bank.repository.model.User;
 
 @Service // IoC의 대상 (싱글톤으로 관리) - 제어의 역전
 public class UserService {
@@ -50,7 +52,23 @@ public class UserService {
 		if(result != 1) {
 			throw new DataDeliveryException("회원가입 실패", HttpStatus.INTERNAL_SERVER_ERROR);
 		}
+	}
+	
+	public User readUser(SignInDTO dto) {
+		// 유효성 검사는 Controller 에서 먼저 하자.
+		User userEntity = null;
+		try {
+			userEntity = userRepository.findByUsernameAndPassword(dto.getUsername(), dto.getPassword());
+		} catch (DataAccessException e) {
+			throw new DataDeliveryException("잘못된 처리 입니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (Exception e) {
+			throw new RedirectException("알수 없는 오류", HttpStatus.SERVICE_UNAVAILABLE);
+		}
 		
+		if(userEntity == null) {
+			throw new DataDeliveryException("아이디 혹은 비밀번호가 틀렸습니다.", HttpStatus.BAD_REQUEST);
+		}
 		
+		return userEntity;
 	}
 }
